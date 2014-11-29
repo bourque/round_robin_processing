4c9702e6ec1fded13eb844be6f804644ed38eae6
Matthew Bourque <bourque@stsci.edu>
2014-11-29 12:12:38 -0500

    Converted tabs to spaces, which is the better thing to use.

--- src/round_robin_processing/Process.java
+++ src/round_robin_processing/Process.java
@@ -1,22 +1,22 @@
-package round_robin_processing;
+//package round_robin_processing;
 
 public class Process {
 
-	String pid;
-	double burstTime;
-	double executionTime;
-	double waitTime;
-	double turnaroundTime;
+    String pid;
+    double burstTime;
+    double executionTime;
+    double waitTime;
+    double turnaroundTime;
 
-	public Process() {
-		/*
-		 * Constructor method.
-		 */
+    public Process() {
+        /*
+         * Constructor method.
+         */
 
-		pid = "1";
-		burstTime = 0.0;
-		executionTime = 0.0;
-		waitTime = 0.0;
-		turnaroundTime = 0.0;
-	}
+        pid = "1";
+        burstTime = 0.0;
+        executionTime = 0.0;
+        waitTime = 0.0;
+        turnaroundTime = 0.0;
+    }
 }
